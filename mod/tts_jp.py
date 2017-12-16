59570776d0467022d6857a632491b7c4ff88ecf6
senyoltw <kumanoroberuto@gmail.com>
2017-12-17 04:47:30 +0900

    add mod and sample

---
+++ mod/tts_jp.py
@@ -0,0 +1,38 @@
+"""Wrapper around a open jtalk system."""
+
+import logging
+import os
+import subprocess
+import tempfile
+import aiy.audio
+
+TMP_DIR = '/run/user/%d' % os.getuid()
+voice = '/usr/share/hts-voice/nitech-jp-atr503-m001/nitech_jp_atr503_m001.htsvoice'
+
+def say(words, voice,  player=aiy.audio.get_player()):
+    """Say the given words with TTS.
+    Args:
+      player: To play the text-to-speech audio.
+      words: string to say aloud.
+    """
+    try:
+        (fd, tts_wav) = tempfile.mkstemp(suffix='.wav', dir=TMP_DIR)
+    except IOError:
+        logger.exception('Using fallback directory for TTS_JP output')
+        (fd, tts_wav) = tempfile.mkstemp(suffix='.wav')
+    os.close(fd)
+
+    dic   = '/var/lib/mecab/dic/open-jtalk/naist-jdic'
+    #voice = '/usr/share/hts-voice/nitech-jp-atr503-m001/nitech_jp_atr503_m001.htsvoice'
+    speed = '1.3'
+
+    try:
+        c = subprocess.Popen(['open_jtalk', '-x', dic, '-m', voice, '-ow', tts_wav, '-r', str(speed)], stdin=subprocess.PIPE)
+        c.stdin.write(words.encode())
+        c.stdin.close()
+        c.wait()
+        player.play_wav(tts_wav)
+    finally:
+        os.unlink(tts_wav)
+
+
---
+++ snowboy_demo.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+# Copyright 2017 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""A demo of the Google CloudSpeech recognizer."""
+
+import aiy.audio
+import aiy.cloudspeech
+import aiy.i18n
+import mod.snowboydecoder as snowboydecoder
+import os
+import sys
+
+aiy.i18n.set_language_code('ja-JP')
+
+if len(sys.argv) == 1:
+    print("Error: need to specify model name")
+    print("Usage: python demo.py your.model")
+    sys.exit(-1)
+
+model = sys.argv[1]
+
+def callbacks():
+    snowboydecoder.play_audio_file(snowboydecoder.DETECT_DING)
+    global interrupted
+    interrupted = True
+
+def interrupt_callback():
+    global interrupted
+    return interrupted
+
+def main():
+    detector = snowboydecoder.HotwordDetector(model, sensitivity=0.5)
+    recognizer = aiy.cloudspeech.get_recognizer()
+    aiy.audio.get_recorder().start()
+
+    while True:
+        print('Speak Wake Word and speak')
+
+        global interrupted
+        interrupted = False
+
+        detector.start(detected_callback=callbacks,
+                       interrupt_check=interrupt_callback,
+                       sleep_time=0.03)
+
+        print('Listening...')
+        text = recognizer.recognize()
+        if not text:
+            print('Sorry, I did not hear you.')
+        else:
+            print('You said "', text, '"')
+
+if __name__ == '__main__':
+    main()
+
